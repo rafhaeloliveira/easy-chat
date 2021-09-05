07afbc63089aaf346dc42766ed86ec27827a5c20
rafhaeloliveira <rafhaf.oliveira@gmail.com>
2021-09-05 11:35:13 -0300

    adicionado hosting do firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,27 @@ import "./index.css";
 import Routes from "./Routes";
 import reportWebVitals from "./reportWebVitals";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBWUSvpWnMj8QAEaX9L9z5JuwnKv5XF9lE",
+  authDomain: "easy-chat-b35ce.firebaseapp.com",
+  projectId: "easy-chat-b35ce",
+  storageBucket: "easy-chat-b35ce.appspot.com",
+  messagingSenderId: "344275224801",
+  appId: "1:344275224801:web:e0b32b32504b94d9280fa0",
+  measurementId: "G-0YGHEVE69G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <Routes />
